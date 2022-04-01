502975f672039df357721c12bdc78645305482a2
0xBronko <paulschulz90@gmail.com>
2022-04-01 06:51:33 +0200

    updating directory page

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
 import ReactDOMClient from "react-dom/client";
 
 const root = ReactDOMClient.createRoot(document.getElementById("root"));
